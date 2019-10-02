d10c1377fe3ba618e5ad62151336d1b54e7658d3
Rohit3475 <rohit170103057@iitg.ac.in>
2019-10-02 14:29:02 +0530

    2 links

---
+++ simulation_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rohit/simulation_ws/src
+# Build directory: /home/rohit/simulation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mrm_description")
---
+++ simulation_ws/build/mrm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rohit/simulation_ws/src/mrm_description
+# Build directory: /home/rohit/simulation_ws/build/mrm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
